7d7fb80cc4c1f707467905d36731fdcd1125a522
Eslam Shaker <binshakerr@gmail.com>
2022-06-20 13:18:17 +0400

    project start

---
+++ EventTickets/EventTickets/EventTicketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventTicketsApp.swift
+//  EventTickets
+//
+//  Created by Eslam Shaker on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct EventTicketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
